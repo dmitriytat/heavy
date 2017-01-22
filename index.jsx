5a15736e57a6d0bf9b53e018ef514dbb6eed9f1e
Dmitriy Tatarintsev <tatarintsevd@gmail.com>
2017-01-22 17:04:20 +0300

    init

---
+++ index.jsx
@@ -0,0 +1,5 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<Application />, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: {
+        index: "./index.jsx",
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist',
+        filename: "[name].bundle.js",
+        chunkFilename: "[id].bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!sass-loader',
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file?name=[path][name].[ext]",
+            }
+        ],
+    },
+    devtool: "eval",
+    plugins: [],
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+            }
+        }
+    }
+};
